5c25c9f7d8065e9c70f998e56dc0136819a54c47
Lowji (Ninja Wolf) <leducloi06112000@gmail.com>
2025-12-16 10:47:16 +0700

    Map 2 - Complete Base Generator

---
+++ Assets/Scripts/Map/Map 2/DungeonSO.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "DungeonSO", menuName = "MapSO/Map02/DungeonSO")]
+public class DungeonSO : ScriptableObject
+{
+    public Vector2Int startIndex;
+    public Vector2Int endIndex;
+
+    public void ResetData()
+    {
+        startIndex = Vector2Int.zero;
+        endIndex = Vector2Int.zero;
+    }
+}
+
+
