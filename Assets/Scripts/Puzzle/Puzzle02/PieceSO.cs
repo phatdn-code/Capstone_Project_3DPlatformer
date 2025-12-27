e6ae2cc380f52139cd480912b3e33f42211d485a
milobuchino1792003 <hominhtam1792003@gmail.com>
2025-12-27 20:14:39 +0700

    remove puzzle
    xóa puzzle để tránh trùng

--- Assets/Scripts/Puzzle/Puzzle02/PieceSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PuzzleSO", menuName = "Puzzle/PieceSO", order = 1)]
-public class PieceSO : ScriptableObject
-{
-    public int x;
-    public int y;
-    public Sprite pieceImage;
-}
