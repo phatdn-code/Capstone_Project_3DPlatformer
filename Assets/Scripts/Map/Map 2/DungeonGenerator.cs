6d293307cbc6c675b127517f9396a5225d3c8b1d
Lowji (Ninja Wolf) <leducloi06112000@gmail.com>
2026-01-18 13:23:01 +0700

    Rewrite DungeonGenerator Script
    Viết lại hệ thống thuật toán sinh hầm ngục tự động

--- Assets/Scripts/Map/Map 2/DungeonGenerator.cs
+++ Assets/Scripts/Map/Map 2/DungeonGenerator.cs
@@ -6,11 +6,13 @@ public class DungeonGenerator : MonoBehaviour
 {
     public int roomLength = 10; // số phòng theo chiều dài
     public float roomSize = 30f; // đúng bằng size prefab
+    public int maxRoomCount = 30;
     public GameObject roomPrefab;
-    public GameObject roadPrefab;
-    public enum CellType { Empty, Room, Road }
+    public enum CellType { Empty, Room}
     [SerializeField] private DungeonSO dungeonDataSO;
 
+    private int minRoomCount = 4;
+    private int roomCount = 0;
     private CellType[,] dungeons;
     private List<Vector2Int> roomList = new List<Vector2Int>();
 
@@ -18,52 +20,48 @@ public class DungeonGenerator : MonoBehaviour
 
     void Start()
     {
-        StartGenerate();
+        ClearDungeon();
+        RoomCount();
+        ChooseStartRoom();
     }
 
-    void StartGenerate()
+    void ClearDungeon()
     {
         dungeonDataSO.ResetData();
         dungeons = new CellType[roomLength, roomLength];
         roomList.Clear();
+    }
+    void RoomCount()
+    {
+
+        if (maxRoomCount <= (roomSize * roomSize))
+        {
+            roomCount = Random.Range(minRoomCount, maxRoomCount);
+        }
+        else
+        {
+            roomCount = Random.Range(minRoomCount, roomLength * roomLength);
+        }
+        Debug.Log("Total Room Count: " + roomCount);
+
+
+    }
+
+    void ChooseStartRoom()
+    {
         int indexX = Random.Range(0, roomLength);
         int indexY = Random.Range(0, roomLength);
         dungeonDataSO.startIndex = new Vector2Int(indexX, indexY);
         SpawnRoom(dungeonDataSO.startIndex);
-        NextGenerate(dungeons[indexX, indexY], dungeonDataSO.startIndex, new Vector2Int(-1,-1));
-    }
-    void NextGenerate(CellType lastCellType, Vector2Int index, Vector2Int lastRoomIndex)
-    {
-        switch (lastCellType)
-        {
-            case CellType.Room:
-                RoadGenerate(lastCellType, index);
-                break;
-            case CellType.Road:
-                RoomGenerate(lastCellType, index,lastRoomIndex);
-                break;
-            case CellType.Empty: return;
-        }
+        roomCount--;
+        NextGenerate(dungeons[indexX, indexY], dungeonDataSO.startIndex);
     }
 
-    bool InBounds(Vector2Int pos)
+
+    void NextGenerate(CellType lastCellType, Vector2Int index)
     {
-        return pos.x >= 0 && pos.x < roomLength 
-            && pos.y >= 0 && pos.y < roomLength;
-    }
-    void RoomGenerate(CellType lastCellType, Vector2Int index, Vector2Int lastIndex)
-    {
-        if(!InBounds(lastIndex)) return;
-        if (lastCellType != CellType.Road) return;
-        Vector2Int availableDirection = 2* index - lastIndex;
-        if(!InBounds(availableDirection)) return;
-        SpawnRoom(availableDirection);
-        NextGenerate(dungeons[availableDirection.x, availableDirection.y], availableDirection,index); 
-    }
-    void RoadGenerate(CellType lastCellType, Vector2Int index)
-    {
-        if (lastCellType != CellType.Room) return;
-        Vector2Int[] availableDirection = CheckRoadDirection(index);
+        if (roomCount <= 0) return;
+        Vector2Int[] availableDirection = CheckDirection(index);
         if (availableDirection.Length == 0) return;
         else
         {
@@ -78,26 +76,34 @@ public class DungeonGenerator : MonoBehaviour
             }
             for (int i = 0; i < count; i++)
             {
-                SpawnRoad(availableDirection[i],index);
-                NextGenerate(dungeons[availableDirection[i].x, availableDirection[i].y], availableDirection[i],index);
+                roomCount--;
+                if (roomCount < 0) return;
+                SpawnRoom(availableDirection[i]);
+                NextGenerate(dungeons[availableDirection[i].x, availableDirection[i].y], availableDirection[i]);
             }
         }
     }
-    Vector2Int[] CheckRoadDirection(Vector2Int index)
+
+    bool InBounds(Vector2Int pos)
+    {
+        return pos.x >= 0 && pos.x < roomLength 
+            && pos.y >= 0 && pos.y < roomLength;
+    }
+    Vector2Int[] CheckDirection(Vector2Int index)
     {
         List<Vector2Int> availDirs = new List<Vector2Int>();
         foreach (var dir in directions)
         {
-            if (InBounds(index + 2 * dir))
+            if (InBounds(index + 1 * dir))
             {
-                if((dungeons[index.x + dir.x, index.y + dir.y] == CellType.Empty) &&
-                   (dungeons[index.x + 2 * dir.x, index.y + 2 * dir.y] == CellType.Empty))
+                if(dungeons[index.x + dir.x, index.y + dir.y] == CellType.Empty)
                 {
                     availDirs.Add(index + dir);
                 }
             }
         }
         Vector2Int[] availableDirection = availDirs.ToArray();
+
         return availableDirection;
     }
     void SpawnRoom(Vector2Int pos)
@@ -109,27 +115,5 @@ public class DungeonGenerator : MonoBehaviour
         roomList.Add(pos);
 
         Instantiate(roomPrefab, new Vector3(pos.x * roomSize, 0, pos.y * roomSize), Quaternion.identity, transform);
-    }
-
-    void SpawnRoad(Vector2Int pos, Vector2Int lastPos)
-    {
-        if (!InBounds(pos)) return;
-        if (dungeons[pos.x, pos.y] != CellType.Empty) return;
-        Quaternion rotation = Quaternion.identity;
-        dungeons[pos.x, pos.y] = CellType.Road;
-        if(RoadCheck(pos,lastPos))
-        {
-            rotation = Quaternion.Euler(0, 90, 0);
-        }
-        Instantiate(roadPrefab, new Vector3(pos.x * roomSize, 0, pos.y * roomSize), rotation, transform);
-    }
-
-    bool RoadCheck(Vector2Int pos, Vector2 lastPos)
-    {
-        if ( (pos.x != lastPos.x) && (pos.y == lastPos.y))
-        {
-            return false;
-        }
-        return true;
     }
 }
