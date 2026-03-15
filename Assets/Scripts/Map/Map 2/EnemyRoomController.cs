586d8335580b4cbf2f3679450c341cf749c5ca2f
Lowji (Ninja Wolf) <leducloi06112000@gmail.com>
2026-03-15 20:06:49 +0700

    Complete Map 2 Without Effect And Teleport

--- Assets/Scripts/Map/Map 2/EnemyRoomController.cs
+++ Assets/Scripts/Map/Map 2/EnemyRoomController.cs
@@ -1,6 +1,44 @@
+using NUnit.Framework;
+using PLAYERTWO.PlatformerProject;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyRoomController : MonoBehaviour
 {
-    [SerializeField] private GameObject n;
+    [SerializeField] private RoomManager roomManager;
+    [SerializeField] private List<EntityController> enemyList = new List<EntityController>();
+
+    private void Start()
+    {
+        roomManager = transform.parent.GetComponentInParent<RoomManager>();
+        if (roomManager != null)
+        {
+            roomManager.isRoomCleared = false;
+        }
+        
+    }
+    private void Update()
+    {
+        if (enemyList.Count > 0)
+        {
+            RemoveEnemy();
+        }
+        else
+        {
+            GameClear();
+        }
+    }
+    private void RemoveEnemy()
+    {
+        enemyList.RemoveAll(enemy => enemy == null || enemy.enabled == false);
+    }
+    private void GameClear()
+    {
+        if (roomManager != null)
+        {
+            roomManager.isRoomCleared = true;
+            roomManager.OpenAllDoors();
+            this.enabled = false;
+        }
+    }
 }
